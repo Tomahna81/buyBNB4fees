d422af2eaba30130202ee41ff7a88b6dbbd7f6b5
Anhamot <36930490+Tomahna81@users.noreply.github.com>
2020-10-10 08:07:47 +0900

    Update recon_script_buybnbeth.sh

--- recon_script_buybnbeth.sh
+++ recon_script_buybnbeth.sh
@@ -2,12 +2,10 @@
 COUNTER=1
 while true; do
 	echo Counter $COUNTER
-	#python3.6 hello_test.py
-	python3.6 buy4fees.py > log.log
+	python buy4fees.py > log.log
 	echo The main program exited... will restart in 60 seconds... If you want to leave, type Ctrl+C in that interval
-	#./send_python_exit.sh
+	#./send_mail_exit.sh
 	sleep 60s
 	COUNTER=`expr $COUNTER + 1`
-	#test $? -gt 128 && break;
 	echo retry...
 done
